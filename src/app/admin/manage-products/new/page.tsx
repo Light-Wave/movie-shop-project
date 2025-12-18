8c31c4fa9c040d509029a4227533f3238fe8d3fc
nikkella26-jpg <nikkella26@gmail.com>
2025-12-18 09:22:40 +0100

    promise

--- src/app/admin/manage-products/new/page.tsx
+++ src/app/admin/manage-products/new/page.tsx
@@ -2,8 +2,9 @@ import { prisma } from "@/lib/prisma";
 import MovieCreateForm from "../../../../components/Forms/movie-form-client";
 
 export default async function NewMoviePage() {
-  const artists = await prisma.artist.findMany();
-  const genres = await prisma.genre.findMany();
+  const artistsPromise = prisma.artist.findMany();
+  const genresPromise = prisma.genre.findMany();
+  const [artists, genres] = await Promise.all([artistsPromise, genresPromise]);
 
   return <MovieCreateForm artists={artists} genres={genres} />;
 }
