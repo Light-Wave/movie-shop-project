1ca12972ea25741ca900d907a080154cecee339c
Light-Wave <66904273+Light-Wave@users.noreply.github.com>
2025-12-29 11:19:32 +0100

    Fixes (#28)

--- src/app/admin/manage-products/page.tsx
+++ src/app/admin/manage-products/page.tsx
@@ -6,7 +6,7 @@ export default async function ManageProductsPage() {
   const movies = await prisma.movie.findMany({
     include: {
       genres: true,
-      movieArtists: {
+      movieLinks: {
         include: {
           artist: true,
         },
@@ -24,7 +24,10 @@ export default async function ManageProductsPage() {
       </div>
       <div className="space-y-2">
         {movies.map((movie) => (
-          <div key={movie.id} className="flex items-center justify-between p-2 border rounded-md">
+          <div
+            key={movie.id}
+            className="flex items-center justify-between p-2 border rounded-md"
+          >
             <div>{movie.title}</div>
             <Button asChild variant="outline">
               <Link href={`/admin/manage-products/${movie.id}`}>Edit</Link>
