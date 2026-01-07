63b1e03b06a7832359965328f5eb1943ab5b769d
nikkella26-jpg <nikkella26@gmail.com>
2026-01-07 10:34:10 +0100

    search bar
    have added wheel to search with working arrow keys and mouse

---
+++ src/server-actions/movie/search-movies.ts
@@ -0,0 +1,28 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+
+export async function getMovieSuggestions(query: string) {
+  if (!query) {
+    return [];
+  }
+
+  try {
+    const movies = await prisma.movie.findMany({
+      where: {
+        title: {
+          contains: query,
+          mode: "insensitive",
+        },
+      },
+      take: 5,
+      select: {
+        id: true,
+        title: true,
+      },
+    });
+    return movies;
+  } catch (error) {
+    console.error("Failed to fetch movie suggestions:", error);
+    return [];
+  }
+}
